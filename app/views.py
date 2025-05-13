8cdef5a4ae24906e40d60ee08ef9da4ecbf54deb
15186AS <15186@eans-nt.edu.pl>
2025-05-13 09:55:41 +0200

    dodanie opcji pobierania opinii

--- app/views.py
+++ app/views.py
@@ -1,7 +1,10 @@
 import os
 import json
 from app import app
-from flask import render_template, redirect, url_for, request
+import io
+import csv
+import pandas as pd
+from flask import render_template, redirect, url_for, request, Flask, send_file, make_response, jsonify
 
 @app.route('/')
 def index():
@@ -44,3 +47,41 @@ def product(product_id):
     else:
         opinions_data = []
     return render_template("product.html", product=product_data, opinions=opinions_data)
+
+@app.route('/download/<product_id>/<format>')
+def download_file(product_id, format):
+    opinions_path = os.path.join("./app/data/opinions", f"{product_id}.json")
+    if not os.path.exists(opinions_path):
+        return "Brak danych opinii dla tego produktu.", 404
+
+    with open(opinions_path, "r", encoding="utf-8") as f:
+        opinions = json.load(f)
+
+    if not opinions:
+        return "Brak opinii do eksportu.", 400
+
+    if format == 'json':
+        return jsonify(opinions)
+
+    elif format == 'csv':
+        output = io.StringIO()
+        writer = csv.DictWriter(output, fieldnames=opinions[0].keys())
+        writer.writeheader()
+        writer.writerows(opinions)
+        response = make_response(output.getvalue())
+        response.headers['Content-Disposition'] = f'attachment; filename=product_{product_id}.csv'
+        response.mimetype = 'text/csv'
+        return response
+
+    elif format == 'xlsx':
+        df = pd.DataFrame(opinions)
+        output = io.BytesIO()
+        with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+            df.to_excel(writer, index=False, sheet_name='Opinie')
+        output.seek(0)
+        return send_file(output,
+                         mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                         download_name=f'product_{product_id}.xlsx',
+                         as_attachment=True)
+
+    return "Nieznany format", 400
