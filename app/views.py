8590a9be3f036de56d198581c0a098168bb99ec7
15186AS <15186@eans-nt.edu.pl>
2025-07-02 23:21:14 +0200

    1

--- app/views.py
+++ app/views.py
@@ -1,10 +1,51 @@
 import os
 import json
+import requests
 from app import app
 import io
+from bs4 import BeautifulSoup
 import csv
+import requests
 import pandas as pd
 from flask import render_template, redirect, url_for, request, Flask, send_file, make_response, jsonify
+
+def list_to_html(l):
+    return "<ul>" + "".join([f"<li>{e}</li>" for e in l]) + "</ul>" if l else ""
+
+def extract(ancestor, selector, attribute=None, multiple=False):
+    if selector:
+        if multiple:
+            if attribute:
+                return [tag[attribute].strip() for tag in ancestor.select(selector)]
+            return [tag.text.strip() for tag in ancestor.select(selector)]
+        if attribute:
+            try:
+                return ancestor.select_one(selector)[attribute].strip()
+            except TypeError:
+                return None
+        try:
+            return ancestor.select_one(selector).text.strip()
+        except AttributeError:
+            return None
+    if attribute:
+        return ancestor[attribute].strip()
+    return None
+
+selectors = {
+    "opinion_id": (None,"data-entry-id"),
+    "author":("span.user-post__author-name",),
+    "recommendation":("span.user-post__author-recomendation > em",),
+    "stars":("span.user-post__score-count",),
+    "content":("div.user-post__text",),
+    "pros":("div.review-feature__item--positive",None,True),
+    "cons":("div.review-feature__item--negative",None,True),
+    "useful":("button.vote-yes","data-total-vote",),
+    "useless":("button.vote-no","data-total-vote",),
+    "post_date":("span.user-post__published > time:nth-child(1)","datetime"),
+    "purchase_date":("span.user-post__published > time:nth-child(2)","datetime")
+    }                         
+
+
 
 @app.route('/')
 def index():
@@ -13,7 +54,36 @@ def index():
 @app.route('/extract', methods=['post'])
 def extract_data():
     product_id = request.form.get('product_id')
-    return redirect(url_for('product', product_id=product_id))
+    url = f"https://www.ceneo.pl/{product_id}#tab=reviews"
+    response = requests.get(url)
+    if response.status_code==200:
+            page_dom = BeautifulSoup(response.text, "html.parser")
+            opinions = page_dom.select("div.js_product-review:not(.user-post--highlighted)")
+            if opinions:
+                all_opinions=[]
+                for opinion in opinions:
+                    single_opinion = {
+                        key: extract(opinion, value,) for key, value in selectors.items()
+                    }
+                    all_opinions.append(single_opinion)
+                try:
+                    url = "https://www.ceneo.pl"+extract(page_dom, "a.pagination__next", "href")
+                except TypeError:
+                    url = None
+                if not os.path.exists("./app/data"):
+                    os.mkdir(".app/data")
+                if not os.path.exists("./app/data/opinions"):
+                    os.mkdir(".app/data/opinions")
+                with open(f"./app/data/opinions/{product_id}.json","w", encoding="UTF-8") as jf:
+                    json.dump(all_opinions, jf, indent=4, ensure_ascii=False)
+                return redirect(url_for('product', product_id=product_id))
+            else:
+                error = "dla produktu o podanym kodzie nie ma opinii"
+                return render_template("extract.html", error=error)
+        
+    error = "cos poszlo nie tak"
+    return render_template("extract.html", error=error)
+
 
 @app.route('/extract', methods=['get'])
 def display_form():
@@ -35,18 +105,19 @@ def author():
 
 @app.route('/product/<product_id>')
 def product(product_id):
-    product_path = os.path.join("./app/data/products", f"{product_id}.json")
-    if not os.path.exists(product_path):
-        return f"Produkt o ID {product_id} nie istnieje.", 404
-    with open(product_path, "r", encoding="utf-8") as f:
-        product_data = json.load(f)
-    opinions_path = os.path.join("./app/data/opinions", f"{product_id}.json")
-    if os.path.exists(opinions_path):
-        with open(opinions_path, "r", encoding="utf-8") as f:
-            opinions_data = json.load(f)
-    else:
-        opinions_data = []
-    return render_template("product.html", product=product_data, opinions=opinions_data)
+    try:
+        with open(f"./app/data/opinions/{product_id}.json", "r", encoding="UTF-8") as jf:
+            try:
+                opinions = json.load(jf)
+            except json.JSONDecodeError:
+                error = "Błędny format pliku"
+                return render_template("product.html", error=error)
+    except FileNotFoundError:
+        error = "Dla prodktu o podanym id nie pobrano jeszcze opinii"
+        return render_template("product.html", error=error)
+    opinions = pd.DataFrame.from_dict(opinions)
+    opinions.pros = opinions.pros.apply(list_to_html)
+    opinions.cons = opinions.cons.apply(list_to_html)
 
 @app.route('/download/<product_id>/<format>')
 def download_file(product_id, format):
